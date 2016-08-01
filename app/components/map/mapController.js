385a4825aa21588d6f6ece5883c664c2208a94d9
Keith Moore <keith.m4@gmail.com>
2016-07-31 21:26:41 -0400

    displayed a layer from geoserver wms

--- app/components/map/mapController.js
+++ app/components/map/mapController.js
@@ -1,11 +1,12 @@
 leafletDemo.controller('mapController', ['$window', function($window) {
 	
-	var testMap = $window.L.map('test-map').setView([32.062, -84.924], 15);
+	var testMap = $window.L.map('test-map').setView([33.862, -84.687], 10);
 
-	$window.L.tileLayer('https://api.tiles.mapbox.com/v4/' + projectId + '/{z}/{x}/{y}.png?access_token=' + pubAccessToken, {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
-		maxZoom: 18,
-		id: projectId,
-		accessToken: pubAccessToken
+	$window.L.tileLayer.wms(baseURL, {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+		layers: 'topp:states',
+		format: 'image/png',
+		transparent: true,
+		version: '1.1.0'
 	}).addTo(testMap);
 }]);
---
+++ assets/js/geoserverWms.js
@@ -0,0 +1,2 @@
+// URL for the Geoserver WMS
+const baseURL = "http://ec2-54-210-135-224.compute-1.amazonaws.com:8080/geoserver/wms";
